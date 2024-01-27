284610d1d81a55020962426ea7a37c2265b0cd7c
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-01-28 00:07:35 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,19 @@
 import "../App.css";
 import { useContext } from "react";
 import { GameStateContext } from "../helpers/Contexts";
-
+import { useState } from "react";
+import Quiz from "./Quiz";
+import EndScreen from "./EndScreen";
+ 
 function Menu() {
-  const { gameState, setGameState, userName, setUserName } = useContext(
-    GameStateContext
-  );
+  // Three state variables are declared using the useState hook: gameState, userName, and score.
+  // gameState is initialized with an empty string (""), userName is initialized as an empty string, and score is initialized with the value 0.
+  const [gameState, setGameState] = useState("");
+  const [userName, setUserName] = useState("");
+  const [score, setScore] = useState(0);
+ 
   return (
+
     <div className="Menu">
       <label  style={{color: 'black', fontSize:40}}>Enter Your Name:</label>
       <input
@@ -16,15 +23,35 @@ function Menu() {
           setUserName(event.target.value);
         }}
       />
+      
+      <GameStateContext.Provider
+      value={{
+        gameState,
+        setGameState,
+        userName,
+        setUserName,
+        score,
+        setScore,
+      }}
+  >
+    
+  </GameStateContext.Provider>
+
       <button id="startquiz"
         onClick={() => {
           setGameState("playing");
         }}
-      >
-        Start Quiz
+      >Start Quiz
       </button>
+      {/* {gameState === "Home" && <Home />} */}
+    {gameState === "menu" && <Menu />}
+    {gameState === "playing" && <Quiz />}
+    {gameState === "finished" && <EndScreen />}
     </div>
+
+    
+    
   );
 }
 
-export default Menu;
+export default Menu; 
