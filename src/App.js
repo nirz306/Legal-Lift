0d4b1a01b1a254f92cc6881b188859b6a423994a
Neeraj Magdum <122902068+nirz306@users.noreply.github.com>
2024-01-28 00:05:19 +0530

    Update App.js for debugging purpose

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Menu from "./components/Menu";
-import Quiz from "./components/Quiz";
-import EndScreen from "./components/EndScreen";
-import { useState } from "react";
-import { GameStateContext } from "./helpers/Contexts";
+
+
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
 import Blogs from "./helpers/Blogs";
 import Home from "./Home";
-// import Create from './Create';
-// ['menu', 'playing', 'finished']
+ 
 
 function App() {
-  const [gameState, setGameState] = useState("menu");
-  const [userName, setUserName] = useState("");
-  const [score, setScore] = useState(0);
-
+ 
   return (
-    // {<Router>
-    <div className="App">
+
+    <Router>
+     <div className="App">
         <Navbar/>
-       <GameStateContext.Provider
-        value={{
-          gameState,
-          setGameState,
-          userName,
-          setUserName,
-          score,
-          setScore,
-        }}
-      >
-        {gameState === "Home" && <Home />}
-        {gameState === "menu" && <Menu />}
-        {gameState === "playing" && <Quiz />}
-        {gameState === "finished" && <EndScreen />}
-      </GameStateContext.Provider>
+
+        <div className="content">
+          <Switch>
+            <Route exact path="/Blogs">
+                <Blogs />
+            </Route>
+            <Route exact path="/Let'splay">
+              <Menu />
+            </Route>
+            <Route exact path="/Home">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+
+      
      </div>
 
-    // </Router>
+    </Router>
+    
   );
 }
 
