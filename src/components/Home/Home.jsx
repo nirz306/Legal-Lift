34aa4bbfbfece1dd7a5e830b261c52f1d93a84e7
SanjanaGhorpade <sanjanasghorpade@gmail.com>
2024-03-01 12:27:00 +0530

    Home page edited

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import "./home.css";
 import lawyer from "./char2.png";
 import AboutUs from "../aboutus"; // Import the AboutUs component
+import sitting_girl from "./Girl_sitting_2.png"
+import girl_social from "./Girl_social2.png"
 
 const Home = () => {
   return (
     <>
       <div className="home">
+      <div className="bgimg">
+          <img className="girl" src={girl_social} alt="" />
+        </div>
         <p className="title">
           Unlock Your Potential<p></p>
           with India's Leading<p></p> Legal Minds
         </p>
-        <div className="bgimg">
-          <img className="lawyer" src={lawyer} alt="" />
-        </div>
+        
       </div>
       <div className="wave">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,224L80,208C160,192,320,160,480,165.3C640,171,800,213,960,224C1120,235,1280,213,1360,202.7L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
---
+++ src/components/Home2/Home2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../Home/Home'
+import aboutus from '../aboutus'
+
+const Home2 = () => {
+  return (
+    <div>
+      <Home/>
+      <aboutus/>
+    </div>
+  )
+}
+
+export default Home2
