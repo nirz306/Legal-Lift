ee0283e45b92468e2c07184c3fb3d4d2c76e48fa
Neeraj Magdum <neerajmagdum10@gmail.com>
2024-04-10 15:39:36 +0530

    endscreen score issue resolved

---
+++ src/components/legal trivia/EndScreen.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Questions } from "./Questions";
+import { NavLink } from "react-router-dom";
+
+const EndScreen = ({ score, totalQuestions }) => {
+  console.log("points", score);
+  return (
+    <>
+      <NavLink className="logo" to="/">
+        Legal Lift
+      </NavLink>
+      {/* <div className="EndScreen">
+        <h1>Quiz Finished</h1>
+        <h1>
+          {score} / {totalQuestions}
+        </h1>
+      </div> */}
+    </>
+  );
+};
+
+
+export default EndScreen;
